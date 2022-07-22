205bb333c8022de028d90a340bbda7d0709a3183
rhanielfarias <rhanielfarias@gmail.com>
2022-07-22 16:37:09 -0300

    t1

--- FuncionariosAdministrativos.java
+++ FuncionariosAdministrativos.java
@@ -1,32 +1,31 @@
-public class FuncionariosAdministrativos extends Funcionario{
 
-    private String funcaoAdministrativa;
-  private String senioridade;
+        public class FuncionariosAdministrativos extends Funcionario{
 
-    public FuncionariosAdministrativos(String nome, String cpf, int nr, int ol, double salario, String funcaoAdministrativa, String senioridade) {
-        super(nome, cpf, nr, ol, salario);
-        this.funcaoAdministrativa = funcaoAdministrativa;
-        this.senioridade = senioridade;
-    }
+            private String funcaoAdministrativa;
+          private String senioridade;
 
+            public FuncionariosAdministrativos(String nome, String cpf, int nr, int ol, double salario, double reembolso, String funcaoAdministrativa, String senioridade) {
+                super(nome, cpf, nr, ol, salario, reembolso);
+                this.funcaoAdministrativa = funcaoAdministrativa;
+                this.senioridade = senioridade;
+            }
 
+            public String getSenioridade() {
+                return senioridade;
+            }
 
-    public String getSenioridade() {
-        return senioridade;
-    }
+            public void setSenioridade(String senioridade) {
+                this.senioridade = senioridade;
+            }
 
-    public void setSenioridade(String senioridade) {
-        this.senioridade = senioridade;
-    }
+            public String getFuncaoAdministrativa() {
+                return funcaoAdministrativa;
+            }
 
-    public String getFuncaoAdministrativa() {
-        return funcaoAdministrativa;
-    }
-
-    public void setFuncaoAdministrativa(String funcaoAdministrativa) {
-        this.funcaoAdministrativa = funcaoAdministrativa;
-    }
-
+            public void setFuncaoAdministrativa(String funcaoAdministrativa) {
+                this.funcaoAdministrativa = funcaoAdministrativa;
+            }
 
 
-}
+
+        }
